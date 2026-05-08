d70354f85e34c26a054f3ce847496034ced9d18a
paumateo22 <pau.mateo25012005@gmail.com>
2026-05-08 18:41:55 +0200

    Post Jornada 34 | Pre Jornada 35

--- pasado/cruzado/scripts/motor_cruzado_presente_pasado.py
+++ pasado/cruzado/scripts/motor_cruzado_presente_pasado.py
@@ -133,7 +133,7 @@ def orquestar_cruce(temporadas_dict):
 
 if __name__ == "__main__":
     temporadas_a_procesar = {
-        "T25-26": [32, 32, []]
+        "T25-26": [34, 34, []]
     }
     
     orquestar_cruce(temporadas_a_procesar)
