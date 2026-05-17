0d3ef2e6b01977177f2a22f26c0c23ff41890761
paumateo22 <pau.mateo25012005@gmail.com>
2026-05-17 11:52:24 +0200

    Post Jornada 36 | Pre Jornada 37

--- JaJ/scraping/scripts/controlador.py
+++ JaJ/scraping/scripts/controlador.py
@@ -99,6 +99,6 @@ def ejecutar_extraccion_previa(temporada, jornada):
 
 if __name__ == "__main__":
     temporada = "2025/26"
-    jornada = 36
+    jornada = 37
     
     ejecutar_extraccion_previa(temporada, jornada)
--- pasado/cruzado/scripts/motor_cruzado_presente_pasado.py
+++ pasado/cruzado/scripts/motor_cruzado_presente_pasado.py
@@ -133,7 +133,7 @@ def orquestar_cruce(temporadas_dict):
 
 if __name__ == "__main__":
     temporadas_a_procesar = {
-        "T25-26": [35, 35, []]
+        "T25-26": [36, 36, []]
     }
     
     orquestar_cruce(temporadas_a_procesar)
