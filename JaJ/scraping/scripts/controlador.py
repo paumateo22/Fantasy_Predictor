8c35c72fc1f5fb7dc171db454d9aff12a1431e03
paumateo22 <pau.mateo25012005@gmail.com>
2026-05-22 12:46:17 +0200

    Pre Jornada 38

--- JaJ/scraping/scripts/controlador.py
+++ JaJ/scraping/scripts/controlador.py
@@ -99,6 +99,6 @@ def ejecutar_extraccion_previa(temporada, jornada):
 
 if __name__ == "__main__":
     temporada = "2025/26"
-    jornada = 37
+    jornada = 38
     
     ejecutar_extraccion_previa(temporada, jornada)
